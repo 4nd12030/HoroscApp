61f77baab0dcf662f8bb97ec1f9c116774154b4b
Andrea Aguiire <4ndr3420@gmail.com>
2024-09-01 20:20:03 -0600

    Inicio de la app

---
+++ app/src/main/java/com/cursokotlin/horoscapp/ui/home/MainActivity.kt
@@ -0,0 +1,12 @@
+package com.cursokotlin.horoscapp.ui.home
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import com.cursokotlin.horoscapp.R
+
+class MainActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+    }
+}
