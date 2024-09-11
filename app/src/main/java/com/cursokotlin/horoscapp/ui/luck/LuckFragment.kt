15478d0facc094280a8dc9c8a67054f34e57a235
Andrea Aguiire <4ndr3420@gmail.com>
2024-09-10 20:06:34 -0600

    Agregando la animacion de la prediccion

--- app/src/main/java/com/cursokotlin/horoscapp/ui/luck/LuckFragment.kt
+++ app/src/main/java/com/cursokotlin/horoscapp/ui/luck/LuckFragment.kt
@@ -6,12 +6,17 @@ import android.util.Log
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import android.view.animation.AlphaAnimation
+import android.view.animation.Animation
+import android.view.animation.AnimationUtils
 import android.view.animation.DecelerateInterpolator
 import android.widget.ImageView
 import android.widget.TextView
+import androidx.core.animation.doOnEnd
+import androidx.core.view.isVisible
 import androidx.fragment.app.Fragment
 import androidx.fragment.app.viewModels
-import androidx.lifecycle.ViewModel
+import com.cursokotlin.horoscapp.R
 import com.cursokotlin.horoscapp.databinding.FragmentLuckBinding
 import dagger.hilt.android.AndroidEntryPoint
 import java.util.Random
@@ -25,15 +30,29 @@ class LuckFragment : Fragment() {
     private var _binding: FragmentLuckBinding? = null
     private val binding get() = _binding!!
 
+    private lateinit var preview: View
+    private lateinit var prediction: View
+    private lateinit var ivLuckyCard: ImageView
     private lateinit var ivRoulette: ImageView
-    private lateinit var ivCard: ImageView
-    private lateinit var tvLuck: TextView
+    private lateinit var ivReverseCard: ImageView
+    private lateinit var tvLucky: TextView
+    private lateinit var tvShare: TextView
 
     override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
         super.onViewCreated(view, savedInstanceState)
 
-        ivRoulette = binding.ivRoulette
+        initBinding()
         initUI()
+    }
+
+    private fun initBinding() {
+        ivRoulette = binding.ivRoulette
+        ivReverseCard =  binding.reverseCard
+        preview = binding.preview
+        prediction = binding.prediction
+        ivLuckyCard = binding.ivLuckyCard
+        tvLucky = binding.tvLucky
+        tvShare = binding.tvShare
     }
 
     private fun initUI() {
@@ -52,7 +71,66 @@ class LuckFragment : Fragment() {
 
         animator.duration = 2000
         animator.interpolator = DecelerateInterpolator()
+        animator.doOnEnd{ slideCard() }
         animator.start()
+    }
+
+    private fun slideCard() {
+        val slideUpAnimation = AnimationUtils.loadAnimation(requireContext(), R.anim.slide_up)
+
+        slideUpAnimation.setAnimationListener(object : Animation.AnimationListener{
+            override fun onAnimationStart(animation: Animation?) {
+                ivReverseCard.isVisible = true
+            }
+
+            override fun onAnimationEnd(animation: Animation?) { growCard() }
+
+            override fun onAnimationRepeat(animation: Animation?) { }
+        })
+
+        ivReverseCard.startAnimation(slideUpAnimation)
+
+    }
+
+    private fun growCard() {
+
+        val growCardAnimation = AnimationUtils.loadAnimation(requireContext(), R.anim.grow)
+
+        growCardAnimation.setAnimationListener(object : Animation.AnimationListener {
+            override fun onAnimationStart(animation: Animation?) { }
+
+            override fun onAnimationEnd(animation: Animation?) {
+                ivReverseCard.isVisible = false
+                showPremonitionView()
+            }
+
+            override fun onAnimationRepeat(animation: Animation?) { }
+        })
+
+        ivReverseCard.startAnimation(growCardAnimation)
+
+    }
+
+    private fun showPremonitionView() {
+        val disappearAnimation = AlphaAnimation(1.0f, 0.0f)
+        disappearAnimation.duration = 200
+
+        val appearAnimation = AlphaAnimation(0.0f, 1.0f)
+        appearAnimation.duration = 1000
+
+        disappearAnimation.setAnimationListener(object : Animation.AnimationListener{
+            override fun onAnimationStart(animation: Animation?) { }
+
+            override fun onAnimationEnd(animation: Animation?) {
+                preview.isVisible = false
+                prediction.isVisible = true
+            }
+
+            override fun onAnimationRepeat(animation: Animation?) { }
+        })
+
+        preview.startAnimation(disappearAnimation)
+        prediction.startAnimation(appearAnimation)
     }
 
 
